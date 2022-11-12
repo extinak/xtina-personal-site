814c1c19a3c5e1f9f790450169cbd9dd393c52e5
xtinak <dev@krie.ch>
2022-11-12 14:41:56 -0800

    added main page, pokemon page, and linked to itch games

---
+++ pokemon/script.js
@@ -0,0 +1,195 @@
+var arr = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Caterpie",
+  "Metapod",
+  "Butterfree",
+  "Weedle",
+  "Kakuna",
+  "Beedrill",
+  "Pidgey",
+  "Pidgeotto",
+  "Pidgeot",
+  "Rattata",
+  "Raticate",
+  "Spearow",
+  "Fearow",
+  "Ekans",
+  "Arbok",
+  "Pikachu",
+  "Raichu",
+  "Sandshrew",
+  "Sandslash",
+  "Nidoran♀",
+  "Nidorina",
+  "Nidoqueen",
+  "Nidoran♂",
+  "Nidorino",
+  "Nidoking",
+  "Clefairy",
+  "Clefable",
+  "Vulpix",
+  "Ninetales",
+  "Jigglypuff",
+  "Wigglytuff",
+  "Zubat",
+  "Golbat",
+  "Oddish",
+  "Gloom",
+  "Vileplume",
+  "Paras",
+  "Parasect",
+  "Venonat",
+  "Venomoth",
+  "Diglett",
+  "Dugtrio",
+  "Meowth",
+  "Persian",
+  "Psyduck",
+  "Golduck",
+  "Mankey",
+  "Primeape",
+  "Growlithe",
+  "Arcanine",
+  "Poliwag",
+  "Poliwhirl",
+  "Poliwrath",
+  "Abra",
+  "Kadabra",
+  "Alakazam",
+  "Machop",
+  "Machoke",
+  "Machamp",
+  "Bellsprout",
+  "Weepinbell",
+  "Victreebel",
+  "Tentacool",
+  "Tentacruel",
+  "Geodude",
+  "Graveler",
+  "Golem",
+  "Ponyta",
+  "Rapidash",
+  "Slowpoke",
+  "Slowbro",
+  "Magnemite",
+  "Magneton",
+  "Farfetch’d",
+  "Doduo",
+  "Dodrio",
+  "Seel",
+  "Dewgong",
+  "Grimer",
+  "Muk",
+  "Shellder",
+  "Cloyster",
+  "Gastly",
+  "Haunter",
+  "Gengar",
+  "Onix",
+  "Drowzee",
+  "Hypno",
+  "Krabby",
+  "Kingler",
+  "Voltorb",
+  "Electrode",
+  "Exeggcute",
+  "Exeggutor",
+  "Cubone",
+  "Marowak",
+  "Hitmonlee",
+  "Hitmonchan",
+  "Lickitung",
+  "Koffing",
+  "Weezing",
+  "Rhyhorn",
+  "Rhydon",
+  "Chansey",
+  "Tangela",
+  "Kangaskhan",
+  "Horsea",
+  "Seadra",
+  "Goldeen",
+  "Seaking",
+  "Staryu",
+  "Starmie",
+  "Mr.Mime",
+  "Scyther",
+  "Jynx",
+  "Electabuzz",
+  "Magmar",
+  "Pinsir",
+  "Tauros",
+  "Magikarp",
+  "Gyarados",
+  "Lapras",
+  "Ditto",
+  "Eevee",
+  "Vaporeon",
+  "Jolteon",
+  "Flareon",
+  "Porygon",
+  "Omanyte",
+  "Omastar",
+  "Kabuto",
+  "Kabutops",
+  "Aerodactyl",
+  "Snorlax",
+  "Articuno",
+  "Zapdos",
+  "Moltres",
+  "Dratini",
+  "Dragonair",
+  "Dragonite",
+  "Mewtwo",
+  "Mew"
+];
+
+var result = document.getElementById('result');
+var whichMon = Math.floor(Math.random() * 151);
+
+var picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  yearRange: [1900,2020]
+});
+
+var seededrandom = null;
+
+function pickAPokeman() { //if seed is not null,
+  //return pokemon
+  //else, if it is null,
+  //check date picker.
+  //if it's null, don't do anything.
+  //otherwise get millis and assign it to seed
+  //then call function again
+
+  console.log("pokemoning!")
+  if (seededrandom == null) {
+    console.log("no seed yet!")
+    console.log(picker.getDate())
+    if (picker.getDate() == null) {} else {
+      console.log("found a date though!")
+      var date = picker.getDate();
+      seededrandom = new Math.seedrandom(date.getTime().toString())
+      console.log("assigned us a randomizer! yay!")
+      pickAPokeman()
+    }
+    //otherwise dont do anything
+  } else {
+    console.log("found seed!")
+    var randomValue = seededrandom()
+    console.log("Random value: " + randomValue)
+    console.log("Pokeman#    : " + randomValue * 151)
+    var whichMon = Math.floor(seededrandom() * 151);
+    result.innerHTML = arr[whichMon];
+    document.getElementById("button").disabled = true;
+  }
+}
+
